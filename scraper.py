9b789e2c7687a0f410b863bd59450ee186d7d60e
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-04-29 00:29:24 +0530

    Made working title scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-def scrapeBlogs():
+def scrapeTitles():
     pageToScrape = requests.get("https://orbitysws.vercel.app/")
-    soup = BeautifulSoup(pageToScrape, "html.parser")
-    titles = soup.findAll('h4', attrs={'class':'text-base'})
+    soup = BeautifulSoup(pageToScrape.text, "html.parser")
+    titles = soup.find_all('h4', attrs={'class':'text-base'})
+
+    for title in titles:
+        print(title.text)
+
+scrapeTitles()

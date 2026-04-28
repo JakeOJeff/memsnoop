69cf0a691112bc23c03002fa32a4dc7c35928295
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-04-29 00:39:12 +0530

    Setting csv returns

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-def scrapeTitles():
+
+with open("books.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Title"]) # uhh u can add other rows asw
     pageToScrape = requests.get("https://orbitysws.vercel.app/")
     soup = BeautifulSoup(pageToScrape.text, "html.parser")
     titles = soup.find_all('h4', attrs={'class':'text-base'})
-
+    
     for title in titles:
-        print(title.text)
-
-scrapeTitles()
+        writer.writerow([title]) 
